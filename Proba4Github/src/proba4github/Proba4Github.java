d63e70c5f7c829a238898566ca71ad9bd3034bd9
rcous <rcous@DESKTOP-M7UFR9H>
2026-03-12 18:26:38 +0100

    Primeiro commit dende Netbeans

---
+++ Proba4Github/src/proba4github/Proba4Github.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proba4github;
+
+/**
+ *
+ * @author rcous
+ */
+public class Proba4Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto foi escrito dende netbeans");
+    }
+    
+}
